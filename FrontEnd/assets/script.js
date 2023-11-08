eb319f67d4e79b9119b397535cd9ae31c5c007e5
laukstl <laurent@kstl.fr>
2023-11-08 11:36:48 +0100

    bouton_cat fonctionnels / ESLint friendly...

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,47 +1,95 @@
-
 async function main () {
-    
-    try{
-        // let response_categories = await fetch("http://localhost:5678/api/categories");
-        // if (response_categories.ok) {
-        //     categories = await response_categories.json();
-        //     console.log(categories.forEach(item=>console.log(item)));
-        // }
-        // else {
-        //     console.error("La requête a échoué. Code : " + response_categories.status);
-        // }
+    let category_selected = 0;
+    let card_listing = {}
+    const gallery_element = document.querySelector(".gallery");
 
+    // Ajout des cards par défaut
+    function show_cards(cards) {
+        clean_gallery()
+        cards.forEach(item => {
+            const card = document.createElement("figure");
+            gallery_element.appendChild(card);
+
+            const img = document.createElement("img");
+            img.src = item.imageUrl;
+            card.appendChild(img);
+
+            const figcaption = document.createElement("figcaption");
+            figcaption.textContent = item.title;
+            card.appendChild(figcaption);
+        })
+    }
+
+    // clean la gallery
+    function clean_gallery() {
+        gallery_element.innerHTML = "";
+    }
+
+    // Update les btn avec le bon background
+    function refresh() {
+        const all_btn = document.querySelectorAll(".btn");
+        all_btn.forEach((button, index) => {
+            if (category_selected == index) {
+                button.classList.add("btn--selected")
+            } else {
+                button.classList.remove("btn--selected")
+            }
+
+            let cards_to_show = {};
+            switch(category_selected) {
+                case 0:
+                    cards_to_show = card_listing;
+                    break;
+                case 1:
+                    cards_to_show = card_listing.filter(item => item.categoryId == 1);
+                    break;
+                case 2:
+                    cards_to_show = card_listing.filter(item => item.categoryId == 2);
+                    break;
+                case 3:
+                    cards_to_show = card_listing.filter(item => item.categoryId == 3);
+                    break;
+            }
+            show_cards(cards_to_show);
+        })
+    }
+
+    try {
         let response_works = await fetch("http://localhost:5678/api/works");
 
-        const gallery_element = document.querySelector(".gallery");
-
         if (response_works.ok) { // 200-299
-            response_works = await response_works.json();
+            card_listing = await response_works.json();
 
-            // récolte des id/noms des catégories
+            // création de la liste des catégories
             let category_tab = {};
-            response_works.forEach(item => {
+            card_listing.forEach(item => {
                 category_tab[item.category.id] = item.category.name;
             });
-            console.log(category_tab);
+            category_tab["0"] = "Tous";
 
-            // Ajout des cards par défaut
-            response_works.forEach(item => {
-                const card = document.createElement("figure");
-                gallery_element.appendChild(card);
+            // creation des boutons de category
+            const category = document.querySelector(".category");
+            for (let i=0; i<Object.keys(category_tab).length; i++) {
+                const btn = document.createElement("button");
+                category.appendChild(btn);
+                const btn_txt = document.createElement("span");
+                btn.appendChild(btn_txt);
+                btn.classList.add("btn");
 
-                const img = document.createElement("img");
-                img.src = item.imageUrl;
-                card.appendChild(img);
+                btn_txt.textContent = category_tab[i];
 
-                const figcaption = document.createElement("figcaption");
-                figcaption.textContent = item.title;
-                card.appendChild(figcaption);
-            })
+                btn.addEventListener("click", () => {
+                    category_selected = i;
+                    refresh();
+                });
+            }   
         }
         else {
             console.error("La requête a échoué. Code : " + response_works.status);
         }
+
+        refresh()
+
     }
     catch(error){
         console.error("Une erreur est survenue : " + error);
