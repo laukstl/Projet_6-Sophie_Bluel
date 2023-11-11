054b37e542cf246eee8331b079b44601504f2f0a
laukstl <laurent@kstl.fr>
2023-11-11 16:07:21 +0100

    Incrustation de la fenetre de login / refactoring

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,4 +1,4 @@
-async function main () {
+async function main_func () {
 
 let category_selected = 0;
 let card_listing = []
@@ -63,6 +63,7 @@ try {
         for (let i=0; i<Object.keys(category_tab).length; i++) {
             const btn = document.createElement("button");
             category.appendChild(btn);
+            btn.style.cursor = "pointer";
             const btn_txt = document.createElement("span");
             btn.appendChild(btn_txt);
             btn.classList.add("btn");
@@ -92,44 +93,91 @@ nav_login.addEventListener("click", () => show_login_screen());
 
 function show_login_screen() {
     const main = document.querySelector("main");
-    // clean screen
+    // clear the main section ( keep header/footer)
     main.innerHTML = "";
 
     // création section LOGIN
-    const section_login = document.createElement("section");
+    const section_login = document.createElement("form");
     main.appendChild(section_login);
     section_login.id = "login";
+    section_login.style.gap = "30px";
+    section_login.style.top = "130px";
+    section_login.style.height = "872px";
+    // section_login.style.display = "none";
+    section_login.style.position = "relative";
 
     // TITRE
     const login_title = document.createElement("h2");
     section_login.appendChild(login_title);
     login_title.innerText = "Log In";
 
+    // Box EMAIL
+    const login_email_box = document.createElement("div");
+    section_login.appendChild(login_email_box);
+    login_email_box.style.display = "flex";
+    login_email_box.style["flex-direction"] = "column";
+    login_email_box.style["align-items"] = "flex-start";
+    login_email_box.style.gap = "7px";
     // input EMAIL
+    const login_email_label = document.createElement("label");
+    login_email_box.appendChild(login_email_label);
+    login_email_label.innerText = "E-mail";
+    login_email_label.style["font-weight"] = "500";
     const login_email = document.createElement("input");
-    section_login.appendChild(login_email);
-    login_email.innerText = "E-mail";
+    login_email_box.appendChild(login_email);
+    login_email.type = "email";
+    login_email.style["font-size"] = "20px";
+    login_email.style["text-align"] = "center";
+    login_email.style.width = "379px";
+    login_email.style.height = "51px";
+    login_email.style.border = "0px";
+    login_email.style.filter = "drop-shadow(0px 3px 15px rgba(0, 0, 0, 0.1))";
 
+    // Box PASSWORD
+    const login_password_box = document.createElement("div");
+    section_login.appendChild(login_password_box);
+    login_password_box.style.display = "flex";
+    login_password_box.style["flex-direction"] = "column";
+    login_password_box.style["align-items"] = "flex-start";
+    login_password_box.style.gap = "7px";
     // input PASSWORD
+    const login_password_label = document.createElement("label");
+    login_password_box.appendChild(login_password_label);
+    login_password_label.innerText = "Mot de passe";
+    login_password_label.style["font-weight"] = "500";
     const login_password = document.createElement("input");
-    section_login.appendChild(login_password);
-    login_password.innerText = "Mot de passe";
+    login_password_box.appendChild(login_password);
+    login_password.type = "password";
+    login_password.style["font-size"] = "20px";
+    login_password.style["text-align"] = "center";
+    login_password.style.width = "379px";
+    login_password.style.height = "51px";
+    login_password.style.border = "0px";
+    login_password.style.filter = "drop-shadow(0px 3px 15px rgba(0, 0, 0, 0.1))";
 
     // BOUTTON
     const login_btn = document.createElement("button");
     section_login.appendChild(login_btn);
     login_btn.style.width = "180px";
+    login_btn.style.height = "51px";
+    login_btn.style.cursor = "pointer";
     const login_btn_span = document.createElement("span")
     login_btn.appendChild(login_btn_span);
     login_btn.classList.add("btn");
     login_btn.classList.add("btn--selected");
     login_btn_span.innerText = "Se connecter";
+    login_btn_span.style["font-size"] = "14px";
+    login_btn.addEventListener("click", () => {
+        alert("Click!");
+    });
 
-    // LIEN FORGOTTEN
+    // lien MDP FORGOTTEN
     const login_mdp_forgotten = document.createElement("a");
     section_login.appendChild(login_mdp_forgotten);
-    section_login.setAttribute("href", "#");
+    login_mdp_forgotten.setAttribute("href", "#");
     login_mdp_forgotten.innerText = "Mot de passe oublié";
+    login_mdp_forgotten.style.color = "inherit";
+    login_mdp_forgotten.style["font-weight"] = "500";
 
     // Affiche la section
     section_login.style.display = "flex";
@@ -137,5 +185,5 @@ function show_login_screen() {
     section_login.style["align-items"] = "center";
 }
 
-} // main()
-main();
+} // main_func()
+main_func();
